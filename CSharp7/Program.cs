a43b49cbce1e75fe05f9c18059528691f4627af3
jmurkoth <jmurkoth@gmail.com>
2016-09-11 16:30:12 -0500

    Added return reference example

--- CSharp7/Program.cs
+++ CSharp7/Program.cs
@@ -18,6 +18,7 @@ namespace Csharp7
             DeconstructDemo();
             DigitSeparatorDemo();
             BinaryLiteralDemo();
+            ReturnRef();
             Console.ReadLine();
         }
       
@@ -149,6 +150,40 @@ namespace Csharp7
             var b = 0b10_01_1100_010_00;
             //should write out 5000;
             Console.WriteLine(b);
+        }
+
+        static void ReturnRef()
+        {
+            Console.WriteLine("****RETURN REFERENCE*****");
+            int[] array = { 1, 15, -39, 0, 7, 14, -12 };
+             ref int place = ref FindasRef(7, array); // aliases 7's place in the array
+            place = 9; // replaces 7 with 9 in the array
+            Console.WriteLine(array[4]); // prints 9
+            int p1 = Find(9, array);
+            p1 = 100;
+            Console.WriteLine(array[4]); // still prints 9
+        }
+        public static  int Find(int number, int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    return  numbers[i]; // return the storage location, not the value
+                }
+            }
+            throw new IndexOutOfRangeException($"{nameof(number)} not found");
+        }
+        public static ref int FindasRef(int number, int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    return ref numbers[i]; // return the storage location, not the value
+                }
+            }
+            throw new IndexOutOfRangeException($"{nameof(number)} not found");
         }
     }
 
