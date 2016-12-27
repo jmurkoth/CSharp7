71e410a97a11bc79f3d4ab1a8c3247bb7afee22d
jmurkoth <jmurkoth@gmail.com>
2016-12-27 14:41:10 -0700

    Updated to include RC1 changes

--- CSharp7/DependentClasses.cs
+++ CSharp7/DependentClasses.cs
@@ -10,54 +10,50 @@ namespace Csharp7
     public class Square : Shape
     {
         public double Length { get; set; }
-        public override double CalculateArea()
-        {
-            return Length * Length;
-        }
-        public override string ToString()
-        {
-            return string.Format($" I am a square with  Length : {Length:#,###.##} ");
-        }
+        public override double CalculateArea() =>  Length * Length;
+
+        public override string ToString() => string.Format($" I am a square with  Length : {Length:#,###.##} ");
     }
 
     public class Circle : Shape
     {
         public double Radius { get; set; }
-        public override double CalculateArea()
-        {
-            return Math.PI * Radius * Radius;
-        }
-        public override string ToString()
-        {
-            return string.Format($" I am a circle with Radius : {Radius:#,###.##} ");
-        }
+        public override double CalculateArea() => Math.PI * Radius * Radius;
+      
+        public override string ToString() => string.Format($" I am a circle with Radius : {Radius:#,###.##} ");
+    
         public void Deconstruct(out double radius, out double area)
         {
             radius = Radius;
             area = CalculateArea();
         }
-        public void AreaPick(out double area)
-        {
-            area = CalculateArea();
-        }
+        public void AreaPick(out double area) => area = CalculateArea();
+     
     }
     public class Rectangle : Shape
     {
         public double Length { get; set; }
         public double Height { get; set; }
-        public override double CalculateArea()
-        {
-            return Length * Height;
-        }
-        public override string ToString()
-        {
-            return string.Format($" I am a Rectangle with Length: {Length:#,###.##} and height  {Height:#,###.##} ");
-        }
+        public override double CalculateArea() => Length * Height;
+      
+        public override string ToString()=> string.Format($" I am a Rectangle with Length: {Length:#,###.##} and height  {Height:#,###.##} ");
+ 
         public void Deconstruct(out double length, out double height, out double area)
         {
             length = Length;
             area = CalculateArea();
             height = Height;
         }
+    }
+
+    public class Bogus : Shape
+    {
+        public int Length { get; set; }
+ 
+        public Bogus(int? length=null)=> Length= length ?? throw new ArgumentNullException(nameof(length));
+       
+
+        public override double CalculateArea() => throw new NotImplementedException();
+      
     }
 }
